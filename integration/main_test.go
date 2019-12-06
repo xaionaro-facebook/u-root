22816f5003d427be05fd2f5d26346c5d9cf94ac2
pallaud <plaud@google.com>
2019-12-06 12:03:42 -0800

    Adding integration test orchestrator
    Problem: we needed to be able to pass a custom initramfs to a test instead of having the test use the generic initramfs.
    
    I couldn’t find a better way of passing the initramfs to a test besides passing it as a flag. But that gets annoying if you  want to run all tests at once, so I wanted to have a test orchestrator that made it easy to run all the tests at once, which also let you specify specific initramfs.
    
    So the way this would work is that main_test.go has a single ‘parent’ test, with all the integration tests running as subtests (this gives us the naming hierarchy so that individual tests can be run as `go test -test.run=TestIntegration/Foo`).  This then allows us to pass in our own initramfs’ (eg. `go test -TestDhclient_Initramfs=PATH -TestScript_Initramfs=PATH`)
    
    Things I don’t like about this: it splits the test into 2 places, an actual test and the entry in the main_test.go table. The env variable feels kind of messy. I wanted to just be able to split out the entire initramfs building into a separate piece but that requires splitting out the test’s u-root build opts again.
    
    If there’s a better way this could be done that would be great!
    
    Signed-off-by: pallaud <plaud@google.com>

---
+++ integration/main_test.go
@@ -0,0 +1,64 @@
+package integration
+
+import (
+	"flag"
+	"os"
+	"testing"
+)
+
+var tests = []struct {
+	name    string
+	runTest func(t *testing.T)
+	// Allow a user to manually pass in an initramfs as a flag for a given test.
+	initramfs *string
+}{
+	// uinit_test.go
+	{
+		name:      "TestHelloWorld",
+		initramfs: flag.String("TestHelloWorld_Initramfs", "", "specify a custom initramfs"),
+		runTest:   RunTestHelloWorld,
+	},
+	{
+		name:      "TestHelloWorldNegative",
+		initramfs: flag.String("TestHelloWorldNegative_Initramfs", "", "specify a custom initramfs"),
+		runTest:   RunTestHelloWorldNegative,
+	},
+	{
+		name:      "TestScript",
+		initramfs: flag.String("TestScript_Initramfs", "", "specify a custom initramfs"),
+		runTest:   RunTestScript,
+	},
+	// dhclient_test.go
+	{
+		name:      "TestDhclient",
+		initramfs: flag.String("TestDhclient_Initramfs", "", "specify a custom initramfs"),
+		runTest:   RunTestDhclient,
+	},
+	{
+		name:      "TestPxeboot",
+		initramfs: flag.String("TestPxeboot_Initramfs", "", "specify a custom initramfs"),
+		runTest:   RunTestPxeboot,
+	},
+	{
+		name:      "TestQEMUDHCPTimesOut",
+		initramfs: flag.String("TestQEMUDHCPTimesOut_Initramfs", "", "specify a custom initramfs"),
+		runTest:   RunTestQEMUDHCPTimesOut,
+	},
+}
+
+func TestIntegration(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.runTest == nil {
+				t.Fatalf("No runner function found for %s", tt.name)
+			}
+
+			// If a custom initramfs was provided by user, prefer that.
+			if len(*tt.initramfs) != 0 {
+				os.Setenv("UROOT_INITRAMFS", *tt.initramfs)
+			}
+
+			tt.runTest(t)
+		})
+	}
+}
